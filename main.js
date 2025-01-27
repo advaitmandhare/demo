00185bee597d9e94a5c66cc14673095b4833114b
advaitmandhare <advaitmandhare@gmail.com>
2025-01-27 10:48:49 +0530

    added comments

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function test(name,ay,by){
-    const age = by-ay;
-    const r = name+"is"+age;
+      const age = by-ay;
+      const r = name+"is"+age;
+      //hello world
     return r;
   }
   console.log(test("alice",1871,1964));
